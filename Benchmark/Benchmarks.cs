e2d430be5f105985ffb210511da751679b2186ab
Kaique Kenji Hirata Silva <kaique.kenji123@gmail.com>
2021-12-28 09:56:08 -0300

    Alterado nomenclatura

--- Benchmark/Benchmarks.cs
+++ Benchmark/Benchmarks.cs
@@ -9,30 +9,30 @@ namespace Benchmark;
 [Orderer(SummaryOrderPolicy.FastestToSlowest)]
 public class Benchmarks
 {
-    private IEnumerable<Portfolio> _portfolios = PortfolioGenerator.GetRandom(1);
-    private static readonly Repository Teste = new Repository();
+    private IEnumerable<Portfolio> _portfolios = PortfolioGenerator.GetRandom(10000);
+    private static readonly Repository _repository = new Repository();
 
     [Benchmark(Baseline = true)]
     public void EPPlus()
     {
-        Teste.CriarPlanilhaEPPlus(_portfolios, @"C:\dev\Excel\Teste.xlsx");
+        _repository.CriarPlanilhaEPPlus(_portfolios, @"C:\dev\Excel\Teste.xlsx");
     }
 
     [Benchmark]
     public void Mapper()
     {
-        Teste.CriarPlanilhaMapper(_portfolios, @"C:\dev\Excel\TesteMapper.xls");
+        _repository.CriarPlanilhaMapper(_portfolios, @"C:\dev\Excel\TesteMapper.xls");
     }
 
     [Benchmark]
     public void Closed()
     {
-        Teste.Write(_portfolios);
+        _repository.Write(_portfolios);
     }
 
     [Benchmark]
     public void Npoi()
     {
-        Teste.CriarPlanilhaNpoi(_portfolios);
+        _repository.CriarPlanilhaNpoi(_portfolios);
     }
 }
